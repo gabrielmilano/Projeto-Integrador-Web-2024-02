0194461f9caed99fa5f3e088452cdf10747f83b9
gabrielmilano <79032997+gabrielmilano@users.noreply.github.com>
2024-11-25 19:46:38 -0300

    build-nova-railway

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\mateu\WebstormProjects\Projeto-Integrador-Web-2024-02\app\layout.tsx
+// File: C:\Users\gabri\OneDrive\Área de Trabalho\Projeto-Integrador-Web-2024-02\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
